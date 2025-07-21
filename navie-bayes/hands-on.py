9892cd601d9e4cab4dbcca91301dd1bd125ba9e1
zafarkhodjakulov <zafarkhodjakulov@gmail.com>
2025-07-21 18:13:21 +0500

    some changes

--- navie-bayes/hands-on.py
+++ navie-bayes/hands-on.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-df = pd.read_csv(r'navie-bayes/spam.csv', encoding='latin-1')
+df = pd.read_csv(r'navie-bayes/spam.csv')
 
 df['label'] = df['Category'].map({'ham': 0, 'spam': 1})
 
@@ -39,16 +39,6 @@ prd = predict_message("You have two cars and a house in your name.")
 print(prd)
 
 
-# def predict_msg(text):
-#     cleaned = text.lower()
-#     cleaned = ''.join([char for char in cleaned if char not in string.punctuation])
-#     words = cleaned.split()
-#     stop_words = stopwords.words('english')
-#     words = [word for word in words if word not in stop_words]
-#     cleaned_text = ' '.join(words)
-    
-#     vec = vectorizer.transform([cleaned_text])
-#     pred = model.predict(vec)[0]
-#     return "📨 SPAM" if pred == 1 else "✅ HAM (oddiy xabar)"
+
 
 
--- navie-bayes/main.py
+++ navie-bayes/main.py
@@ -7,15 +7,12 @@ import string
 import nltk
 from nltk.corpus import stopwords
 
-# NLTK stopwords (birinchi marta ishlatsangiz, quyidagini uncomment qiling)
-# nltk.download('stopwords')
 
-# 1. Ma’lumotni o‘qish
 df = pd.read_csv('spam.csv', encoding='latin-1')
 df = df[['category', 'message']]
 df.columns = ['label', 'text']
 
-# 2. Tozalash funksiyasi
+
 def clean_text(text):
     text = text.lower()
     text = ''.join([char for char in text if char not in string.punctuation])
@@ -25,23 +22,16 @@ def clean_text(text):
     return ' '.join(words)
 
 df['clean_text'] = df['text'].apply(clean_text)
-
-# 3. Labelni raqamlash (spam=1, ham=0)
 df['label_num'] = df['label'].map({'ham': 0, 'spam': 1})
-
-# 4. Train/Test ajratish
 X_train, X_test, y_train, y_test = train_test_split(df['clean_text'], df['label_num'], test_size=0.2, random_state=42)
 
-# 5. TF-IDF vektorlash
+
 vectorizer = TfidfVectorizer()
 X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
 
-# 6. Modelni o‘qitish
 model = MultinomialNB()
 model.fit(X_train_vec, y_train)
-
-# 7. Bashorat va natijalar
 y_pred = model.predict(X_test_vec)
 
 print("📊 Classification Report:")
@@ -51,14 +41,12 @@ print("\n🧩 Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
 
 
 
-# 8. Kiritilgan matnga qarab spam/ham aniqlash funksiyasi
 def predict_message(text):
     cleaned = clean_text(text)
     vec = vectorizer.transform([cleaned])
     pred = model.predict(vec)[0]
     return "📨 SPAM" if pred == 1 else "✅ HAM (oddiy xabar)"
 
-# 🔎 Sinab ko‘ramiz
 while True:
     user_input = input("\n✉️ Xabar kiriting (chiqish uchun 'exit'): ")
     if user_input.lower() == 'exit':
